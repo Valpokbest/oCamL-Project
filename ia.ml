cbe22de1e2e13e1440128d06ffa4c66d2439372b
Valpokbest <lefevre.valentin@gmail.com>
2013-12-17 11:09:31 +0100

    Create ia.ml

---
+++ ia.ml
@@ -0,0 +1,13 @@
+(*#load "graphics.cma";;
+#load "types_et_donnees.cmo";;
+#load "fenetre_graphique.cmo";;
+#load "calculs.cmo";;*)
+(* à décommenter si non compilé *)
+
+open Graphics;;
+open Types_et_donnees;;
+open Fenetre_graphique;;
+open Calculs;;
+
+let action_ia () =
+();;
