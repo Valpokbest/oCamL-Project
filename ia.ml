0ee4c42d8c3fd7c5b1412cc90e339f21094d48c7
Valpokbest <lefevre.valentin@gmail.com>
2013-12-27 18:31:40 +0100

    Update ia.ml

--- ia.ml
+++ ia.ml
@@ -13,18 +13,68 @@ open Generation_de_terrains;;
 
 let collision (i,j) =
   let retour = ref false in
-  if terrain.(i).(j).element = Eau then retour := true;
-  if terrain.(i).(j).pompier > 0 then retour := true;
-  if terrain.(i).(j).intensite_feu > 0 then retour := true;
+  if i<0 || j<0 || i>n || j>m then retour := true;
+  if not(!retour) && terrain.(i).(j).element = Eau then retour := true;
+  if not(!retour) && terrain.(i).(j).pompier > 0 then retour := true;
+  if not(!retour) && terrain.(i).(j).intensite_feu > 0 then retour := true;
   !retour;;
 
-let action_ia_fonce () =
- let liste  = !liste_pompiers in
-
-  let deplace_pompier (x,y) =
-    pompier_x := x;
-    pompier_y := y;
-    let objectifx = ref 0 and objectify = ref 0 in
+let poser_pompier () =
+	print_string("poser_pompier()");
+	print_newline();
+	let max = ref 0 and i = ref 0 and j = ref 0 in
+	for i1=0 to n-1 do
+		for j1=0 to m-1 do
+		if (not(collision (i1,j1))) then
+		begin
+		let homogene = ref 0 in
+		 if (i1>0) then
+		begin
+		  if (j1>0) then
+		if (terrain.(i1-1).(j1-1).intensite_feu > 0) then
+		  incr(homogene);
+		  if (terrain.(i1-1).(j1).intensite_feu > 0) then
+		incr(homogene);
+		  if (j1<m-1) then
+		if (terrain.(i1-1).(j1+1).intensite_feu > 0) then
+		  incr(homogene);
+		end;
+	  if (j1<m-1) then
+		if (terrain.(i1).(j1+1).intensite_feu > 0) then
+		  incr(homogene);
+	  if (i1<n-1) then
+		begin
+		  if (j1<m-1) then
+		if (terrain.(i1+1).(j1+1).intensite_feu > 0) then
+		  incr(homogene);
+		  if (terrain.(i1+1).(j1).intensite_feu > 0) then
+		incr(homogene);
+		  if (j1>0) then
+		if (terrain.(i1+1).(j1-1).intensite_feu > 0) then
+		  incr(homogene);
+		end;
+	  if (i1>0) then
+		if (terrain.(i1-1).(j1).intensite_feu > 0) then
+		  incr(homogene);
+	  if (!homogene > !max) then
+		begin
+			max:=!homogene;
+			i:=i1;
+			j:=j1;
+		end
+	end;
+	done;
+done;
+	liste_pompiers := (!j,!i)::(!liste_pompiers);
+	terrain.(!i).(!j).pompier <- 1;
+	dessine_case (!i) (!j);
+	;;
+	
+	
+let objectif x y =
+	print_string("objectif()");
+	print_newline();
+let objectifx = ref (-1) and objectify = ref (-1) in
     let distance_min = ref (n*m) in
     for i=0 to n-1 do
       for j=0 to m-1 do
@@ -40,7 +90,21 @@ let action_ia_fonce () =
           end;
       done;
     done;
-	print_int(!objectifx);
+	(!objectifx, !objectify);;
+	
+let action_ia_fonce () =
+	print_string("action_ia_fonce()");
+	print_newline();
+ let liste  = !liste_pompiers in
+
+  let deplace_pompier (x,y) =
+    pompier_x := x;
+    pompier_y := y;
+    
+	let (objectifx,objectify) = objectif x y in
+	print_string("Objectif trouvé");
+	print_newline();
+	(*print_int(!objectifx);
 	print_newline();
 	print_int(!objectify);
 	print_newline();
@@ -48,17 +112,20 @@ let action_ia_fonce () =
 	print_newline();
 	print_int(!pompier_y);
 	print_newline();
-	print_newline();
-	let coince = ref (!objectifx = 0 || !objectify = 0) in
+	print_newline();*)
+	let coince = ref (objectifx = (-1) || objectify = (-1)) in
     while (terrain.(!pompier_y).(!pompier_x).pompier < 4 && not(!coince)) do
-    if abs(!objectifx- !pompier_x) > abs(!objectify - !pompier_y) then
+    if abs(objectifx- !pompier_x) > abs(objectify - !pompier_y) then
 	  begin
-      if !objectifx - !pompier_x > 0 then
+      if objectifx - !pompier_x > 0 then
 		begin
+	print_string("Direction Droite");
+	print_newline();
 		if (not(collision(!pompier_y,!pompier_x+1))) then
 			move_pompier Right
 		else
-			if !objectify - !pompier_y > 0 then
+			begin
+			if objectify - !pompier_y > 0 then
 				if (not(collision(!pompier_y+1,!pompier_x))) then
 					move_pompier Down
 				else 
@@ -68,13 +135,17 @@ let action_ia_fonce () =
 					move_pompier Up
 				else
 					coince:=true
+			end
 		end
       else
 		begin
+	print_string("Direction Gauche");
+	print_newline();
 		if (not(collision(!pompier_y,!pompier_x-1))) then
 			move_pompier Left
 		else
-			if !objectify - !pompier_y > 0 then
+			begin
+			if objectify - !pompier_y > 0 then
 				if (not(collision(!pompier_y+1,!pompier_x))) then
 					move_pompier Down
 				else 
@@ -84,16 +155,20 @@ let action_ia_fonce () =
 					move_pompier Up
 				else
 					coince:=true
+			end
 		end
 	  end
     else
 	  begin
-      if !objectify - !pompier_y > 0 then
+      if objectify - !pompier_y > 0 then
 		begin
+	print_string("Direction Bas");
+	print_newline();
         if (not(collision(!pompier_y+1,!pompier_x))) then
 			move_pompier Down
 		else
-			if !objectifx - !pompier_x > 0 then
+			begin
+			if objectifx - !pompier_x > 0 then
 				if (not(collision(!pompier_y,!pompier_x+1))) then
 					move_pompier Right
 				else 
@@ -103,13 +178,17 @@ let action_ia_fonce () =
 					move_pompier Left
 				else
 					coince:=true
+			end
 		end
       else
 		begin
+	print_string("Direction Haut");
+	print_newline();
         if (not(collision(!pompier_y-1,!pompier_x))) then
 			move_pompier Up
 		else
-			if !objectifx - !pompier_x > 0 then
+			begin
+			if objectifx - !pompier_x > 0 then
 				if (not(collision(!pompier_y,!pompier_x+1))) then
 					move_pompier Right
 				else 
@@ -119,6 +198,7 @@ let action_ia_fonce () =
 					move_pompier Left
 				else
 					coince:=true
+			end
 		end
 	  end;
 	done;
@@ -127,7 +207,10 @@ let action_ia_fonce () =
 
   let rec deplacer_pompiers = function
     | [] -> []
-    | t::q -> (deplace_pompier t)::(deplacer_pompiers q)
+    | t::q -> 
+	print_string("deplacer_pompiers()");
+	print_newline();
+	(deplace_pompier t)::(deplacer_pompiers q)
   in
 
 liste_pompiers := deplacer_pompiers liste;;
