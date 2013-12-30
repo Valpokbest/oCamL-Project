1fc09fb99997f2e253826fb7628f95092652003b
Valpokbest <lefevre.valentin@gmail.com>
2013-12-30 21:00:35 +0100

    Update files.ml

--- files.ml
+++ files.ml
@@ -8,72 +8,72 @@ open Generation_de_terrains;;
 open Fenetre_graphique;;
 
 let sauver carte num = 
-	let nb_rows=Array.length carte and nb_cols=Array.length carte.(0) in
-	let fichier = open_out ("Saves/"^string_of_int(num)^".map") in
-	output_string fichier (string_of_int(!compteur_tour));
-	output_string fichier "\n";
-	output_string fichier (string_of_int(!compteur_pompiers));
-	output_string fichier "\n";
-	output_string fichier (string_of_int(nb_rows));
-	output_string fichier "\n";
-	output_string fichier (string_of_int(nb_cols));
-	output_string fichier "\n";
-	for i=0 to nb_rows-1 do
-		for j=0 to nb_cols-1 do
-			let biome = match carte.(i).(j).element with
-				|Eau -> "00"
-				|Foret -> "01"
-				|Plaine -> "02"
-				|Maison -> "03"
-				|Centrale -> "04" in
-			output_string fichier biome;
-			output_string fichier (string_of_int(carte.(i).(j).intensite_feu));
-			if (carte.(i).(j).calcine) then output_string fichier "1" else output_string fichier "0";
-			if (carte.(i).(j).estompe) then output_string fichier "1" else output_string fichier "0";
-			if (carte.(i).(j).brule) then output_string fichier "1" else output_string fichier "0";
-			output_string fichier (string_of_int(carte.(i).(j).pompier));
-			output_string fichier "\n";
-		done;
-	done;
-	close_out fichier;;
-	
+  let nb_rows=Array.length carte and nb_cols=Array.length carte.(0) in
+  let fichier = open_out ("Saves/"^string_of_int(num)^".map") in
+  output_string fichier (string_of_int(!compteur_tour));
+  output_string fichier "\n";
+  output_string fichier (string_of_int(!compteur_pompiers));
+  output_string fichier "\n";
+  output_string fichier (string_of_int(nb_rows));
+  output_string fichier "\n";
+  output_string fichier (string_of_int(nb_cols));
+  output_string fichier "\n";
+  for i=0 to nb_rows-1 do
+    for j=0 to nb_cols-1 do
+      let biome = match carte.(i).(j).element with
+	| Eau -> "00"
+	| Foret -> "01"
+	| Plaine -> "02"
+	| Maison -> "03"
+	| Centrale -> "04" in
+      output_string fichier biome;
+      output_string fichier (string_of_int(carte.(i).(j).intensite_feu));
+      if (carte.(i).(j).calcine) then output_string fichier "1" else output_string fichier "0";
+      if (carte.(i).(j).estompe) then output_string fichier "1" else output_string fichier "0";
+      if (carte.(i).(j).brule) then output_string fichier "1" else output_string fichier "0";
+      output_string fichier (string_of_int(carte.(i).(j).pompier));
+      output_string fichier "\n";
+    done;
+  done;
+  close_out fichier;;
+
 let charger num = 
-	let fichier = open_in ("Saves/"^string_of_int(num)^".map") in
-	compteur_tour := int_of_string(input_line fichier);
-	actualiser_tour ();
-	compteur_pompiers := int_of_string(input_line fichier);
-	actualiser_nombre_pompiers ();
-	let nb_rows = int_of_string(input_line fichier) in
-	let nb_cols = int_of_string(input_line fichier) in
-	for i=0 to nb_rows-1 do
-		for j=0 to nb_cols-1 do
-			let premier_code = String.make 1 (input_char fichier) in
-			let deuxieme_code = String.make 1 (input_char fichier) in
-			let code_biome = premier_code^deuxieme_code in
-			let elt = (match code_biome with
-				|"00" -> Eau
-				|"01" -> Foret
-				|"02" -> Plaine
-				|"03" -> Maison
-				|"04" -> Centrale
-				|_ -> Eau) in
-			let case = init_case(elt) in
-			case.intensite_feu <- int_of_string(String.make 1 (input_char fichier));
-			let c = input_char fichier in
-			case.calcine <- (match c with
-				|'0' -> false
-				|'1' -> true
-				|_ -> false);
-			case.estompe <- (match (input_char fichier) with
-				|'0' -> false
-				|'1' -> true
-				|_ -> false);
-			case.brule <- (match (input_char fichier) with
-				|'0' -> false
-				|'1' -> true
-				|_ -> false);
-			case.pompier <- int_of_string(String.make 1 (input_char fichier));
-			let vider_ligne = input_line fichier in
+  let fichier = open_in ("Saves/"^string_of_int(num)^".map") in
+  compteur_tour := int_of_string(input_line fichier);
+  actualiser_tour ();
+  compteur_pompiers := int_of_string(input_line fichier);
+  actualiser_nombre_pompiers ();
+  let nb_rows = int_of_string(input_line fichier) in
+  let nb_cols = int_of_string(input_line fichier) in
+  for i=0 to nb_rows-1 do
+    for j=0 to nb_cols-1 do
+      let premier_code = String.make 1 (input_char fichier) in
+      let deuxieme_code = String.make 1 (input_char fichier) in
+      let code_biome = premier_code^deuxieme_code in
+      let elt = (match code_biome with
+	| "00" -> Eau
+	| "01" -> Foret
+	| "02" -> Plaine
+	| "03" -> Maison
+	| "04" -> Centrale
+	| _ -> Eau) in
+      let case = init_case(elt) in
+      case.intensite_feu <- int_of_string(String.make 1 (input_char fichier));
+      let c = input_char fichier in
+      case.calcine <- (match c with
+	| '0' -> false
+	| '1' -> true
+	| _ -> false);
+      case.estompe <- (match (input_char fichier) with
+	| '0' -> false
+	| '1' -> true
+	| _ -> false);
+      case.brule <- (match (input_char fichier) with
+	| '0' -> false
+	| '1' -> true
+	| _ -> false);
+      case.pompier <- int_of_string(String.make 1 (input_char fichier));
+      let _ = input_line fichier in
 			terrain.(i).(j) <- case;
-		done;
-	done;;
+    done;
+  done;;
