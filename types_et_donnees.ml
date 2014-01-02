0e2a9eb2c68d5732c51ef91fe716b79b3cfc9791
Valpokbest <lefevre.valentin@gmail.com>
2014-01-02 17:30:28 +0100

    Update types_et_donnees.ml

--- types_et_donnees.ml
+++ types_et_donnees.ml
@@ -1,18 +1,23 @@
 type element = Eau | Foret | Plaine | Maison | Centrale;;
-type direction = Haut | Bas | Gauche | Droite | NO | NE | SO | SE;;
-type mouvement = Up | Down | Left | Right;;
-type action = Pompier | Water | Feu;;
+(* Résolution max de l'écran *)
+let max_x = 1280;;
+let max_y = 1024;;
+
+type element = Eau | Foret | Plaine | Maison | Centrale;; (*Pour le type d'une case*)
+type direction = Haut | Bas | Gauche | Droite | NO | NE | SO | SE;; (*Pour le vent*)
+type mouvement = Up | Down | Left | Right;; (*Pour les mouvements des pompiers*)
+type action = Pompier | Water | Feu;; (*Action de la souris*)
 
 type case = {
-  element : element;
+  element : element;				(*Type de la case*)
   mutable intensite_feu : int;
-  mutable calcine : bool;
-  mutable estompe : bool;
-  mutable brule : bool;
-  mutable pompier : int
+  mutable calcine : bool;			(*true si la case a fini de bruler*)
+  mutable estompe : bool;			(*true si intensite_feu = intensite_max*)
+  mutable brule : bool;				(*true si intensite_feu >= intensite_max/2*)
+  mutable pompier : int				(*0 = pas de pompier sur la case ; sinon valeur-1 = nb de pas effectués durant le tour (cad 4 = pompier coincé)*)
 };;
 
-type terrain = case array array;;
+type terrain = case array array;;	(*matrice pour le terrain*)
 
 (* Densités des terrains : la somme doit valoir 1 *)
 let prob_foret = 0.434;;
@@ -21,24 +26,21 @@ let prob_eau = 0.22;;
 let prob_centrale = 0.002;;
 
 let prob_maison = 0.15;;
+
+(*probabilité que la foudre tombe tous les 10 tours*)
 let prob_foudre = 0.5;;
 
 (* Variables globales pour la simulation *)
-let wind = ref true;;
 let wind_direction = ref SO;;
 let foudre = ref false;;
 let ia = ref false;;
 let compteur_tour = ref 1;;
 let action_souris = ref Pompier;;
-let pompier_x = ref 0;;
+let pompier_x = ref 0;;		(*coords du pompier sélectionné*)
 let pompier_y = ref 0;;
 let compteur_pompiers = ref 0;;
-let liste_pompiers : ((int*int) list) ref = ref [];;
-let fq_pomp = 10;;
-
-(* Résolution max de l'écran *)
-let max_x = 1280;;
-let max_y = 1024;;
+let liste_pompiers : ((int*int) list) ref = ref [];;	(*l'emplacement des pompiers est aussi géré sous forme de liste*)
+let fq_pomp = 10;;	(*Nb de tours avant arrivée d'un pompier*)
 
 (*** Choix de l'utilisateurs ***)
 
@@ -51,26 +53,29 @@ let m = (* colonnes *)
   print_endline "Nombre de colonnes ?";
   read_int();;
 print_endline "Effets du climat :";;
-print_endline "Direction initiale du vent : Nord (n), Nord-Ouest (m), Ouest (o), Sud-Ouest (l), Sud (s), Sud-Est (k), Est (e), Nord-Est (j) ou pas de vent (p)";;
-wind_direction := (match read_line() with
-	| "n" -> Nord
-	| "s" -> Sud
-	| "o" -> Ouest
-	| "e" -> Est
-	| "m" -> NO
-	| "l" -> SO
-	| "k" -> SE
-	| "j" -> NE
-	| _ -> wind := false; Nord);;
-print_endline "Pluie? (y/n)";;
-let rain := ( match read_line() with
+print_endline "Direction initiale du vent : Nord (n), Nord-Ouest (m), Ouest (o), Sud-Ouest (l), Sud (s), Sud-Est (k), Est (e), Nord-Est (j) ou Pas de vent (p)";;
+let wind =
+  let k = ref true in
+  wind_direction := (match read_line() with
+    | "n" -> Haut
+    | "s" -> Bas
+    | "o" -> Gauche
+    | "e" -> Droite
+    | "m" -> NO
+    | "l" -> SO
+    | "k" -> SE
+    | "j" -> NE
+    | _ -> k := false; Haut);
+!k;;
+print_endline "Pluie ? (y/N)";;
+let rain = ( match read_line() with
     | "y" -> true
     | _ -> false);;
-print_endline "Foudre? (y/n)";;
-let thunder := ( match read_line() with
+print_endline "Foudre ? (y/N)";;
+let thunder = ( match read_line() with
     | "y" -> true
     | _ -> false);;
-	
+
 
 let advanced_simulation =
   print_endline "Voulez-vous lancer la simulation avancee (a) ou simple (s) ? (A/s)";
@@ -79,4 +84,4 @@ let advanced_simulation =
     | _ -> print_endline "Simulation avancee"; true
 ;;
 
-let quantite_feu = int_of_float(ceil(float(n*m)/.(100.*.log(float(n*m)))));;
+let quantite_feu = 1;;
