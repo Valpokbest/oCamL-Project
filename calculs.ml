b8b51fad3f026a10b2e54b53494fde38b923ffcd
Valpokbest <lefevre.valentin@gmail.com>
2014-01-02 18:41:06 +0100

    Update calculs.ml

--- calculs.ml
+++ calculs.ml
@@ -9,7 +9,7 @@ open Types_et_donnees;;
 open Generation_de_terrains;;
 open Fenetre_graphique;;
 
-
+(*Fonction pour calculer le score final*)
 let score () = 
   let score = ref 0 in
   for i=0 to n-1 do
@@ -26,6 +26,7 @@ let score () =
   done;
   !score;;
   
+(*Fonction pour calculer le score maximal*)
 let score_max () = 
   let score = ref 0 in
   for i=0 to n-1 do
@@ -41,6 +42,7 @@ let score_max () =
   done;
   !score;;
 
+(*Fonction auxiliaire pour mettre à jour la liste des pompiers*)
 let actualise_pompier x y k l =
   let liste = !liste_pompiers in
 
@@ -52,6 +54,7 @@ let actualise_pompier x y k l =
 
 liste_pompiers := aux liste;;
 
+(*Fonction auxiliaire pour supprimer un pompier de la liste des pompiers*)
 let supprimer_pompier x y =
   let liste = !liste_pompiers in
 
@@ -72,13 +75,14 @@ let allumer_feu case =
     end
 ;;
 
+(*Fonction de triche*)
 let eteindre_feu case =
   case.estompe <- false;
   case.intensite_feu <- 0;
   case.brule <- false;;
 
 let maj_feu case i = (*i=1 pour augmenter feu; i=2 pour diminuer feu*)
-  if (case.pompier = 0 && not(case.calcine)) then
+  if (case.pompier = 0 && not(case.calcine)) then (*on vérifie qu'il n'y a pas de pompier sur la case et qu'elle n'a pas déjà brulée entièrement*)
     begin
       if (i=1) then
 	begin
@@ -109,16 +113,17 @@ let maj_feu case i = (*i=1 pour augmenter feu; i=2 pour diminuer feu*)
     end
 ;;
 
+(*fonction pour gérer l'explosion d'une centrale nucléaire*)
 let explosion carte k l =
   let (coeffx,coeffy) = (0,0) in
   for i=0 to n-1 do
     for j=0 to m-1 do
       if (carte.(i).(j).element <> Eau) then
-	if ((abs (i+coeffx-k) + abs (j+coeffy-l)) < 5) then
+	if ((abs (i+coeffx-k) + abs (j+coeffy-l)) < 5) then (*5 est le rayon d'explosion*)
 	  (carte.(i).(j).intensite_feu <- 0;
 	   carte.(i).(j).calcine <- true;
-	   carte.(i).(j).pompier <- 0;
-	   supprimer_pompier j i)
+	   carte.(i).(j).pompier <- 0;	(*on supprime les pompiers morts de la carte*)
+	   supprimer_pompier j i) 		(*et de la liste*)
 	else if ((abs (i+coeffx-k) + abs (j+coeffy-l)) = 5) then
 	  (allumer_feu carte.(i).(j);
 	   carte.(i).(j).pompier <- 0;
@@ -127,8 +132,14 @@ let explosion carte k l =
     done;
   done;;
 
+(*crée une nouvelle case avec les mêmes valeurs*)
 let clone case = {element = case.element; intensite_feu = case.intensite_feu; estompe = case.estompe; calcine = case.calcine; brule = case.brule; pompier = case.pompier};;
 
+(*calcule le coefficient du au vent pour la simulation*)
+(*i est le numéro de la case voisine selon ce schéma:
+     1  2  3
+	 4  X  6
+	 7  8  9  où X est la case en feu*)
 let coeff_vent i =
   match !wind_direction with
 	| Haut -> if (i < 4) then 0.25 else if (i>6) then -1. else 0.
@@ -140,6 +151,7 @@ let coeff_vent i =
 	| NO -> if (i=4 || i=7 || i=8) then -1. else if (i=2 || i=3 || i=6) then 0.25 else 0.
 	| NE -> if (i=6 || i=8 || i=9) then -1. else if (i=1 || i=2 || i=4) then 0.25 else 0.;;
 
+(*calcule la proba que la case "case" avec le numéro num par rapport à une case en feu (=i de la fonction précédente) brule*)
 let calcul_proba case facteur_distance num =
   let facteur_terrain = ref 0. in
   let intensite_max = ref 0 in
@@ -157,6 +169,7 @@ let calcul_proba case facteur_distance num =
     p := 0.25;
   (!facteur_terrain) *. facteur_distance *. (0.75)**( float_of_int(!intensite_max - case.intensite_feu)) *. (1. +. !v -. !p);;
 
+(*vérifie s'il reste des cases enflammables ou non, pour gérer l'appel aléatoire sur une case de la foudre et éviter les boucles infinies*)
 let tout_en_feu () =
   let check = ref true in
   let i = ref 0 in
@@ -198,8 +211,9 @@ let move_pompier dir =
     | Down -> (!pompier_y)<n && terrain.(!pompier_y+1).(!pompier_x).element != Eau && terrain.(!pompier_y+1).(!pompier_x).pompier = 0 && terrain.(!pompier_y+1).(!pompier_x).intensite_feu = 0
     | Left -> (!pompier_x)>0 && terrain.(!pompier_y).(!pompier_x-1).element != Eau && terrain.(!pompier_y).(!pompier_x-1).pompier = 0 && terrain.(!pompier_y).(!pompier_x-1).intensite_feu = 0
     | Right -> (!pompier_x)<m && terrain.(!pompier_y).(!pompier_x+1).element != Eau && terrain.(!pompier_y).(!pompier_x+1).pompier = 0 && terrain.(!pompier_y).(!pompier_x+1).intensite_feu = 0 in
-  
-  if (terrain.(!pompier_y).(!pompier_x).pompier > 0 && terrain.(!pompier_y).(!pompier_x).pompier < 4 && possible) then
+  (*possible vérifie si la case de destination est atteignable*)
+  if (terrain.(!pompier_y).(!pompier_x).pompier > 0 && terrain.(!pompier_y).(!pompier_x).pompier < 4 && possible) then (*si le pompier peut encore bouger
+																													et s'il y a bien un pompier...*)
     begin
       let (i,j) =
 	match dir with
@@ -209,16 +223,16 @@ let move_pompier dir =
 	  | Right -> (!pompier_y,!pompier_x+1)
       in
       terrain.(i).(j).pompier <- terrain.(!pompier_y).(!pompier_x).pompier + 1;
-      terrain.(!pompier_y).(!pompier_x).pompier <- 0;
+      terrain.(!pompier_y).(!pompier_x).pompier <- 0; (*on déplace le pompier*)
       dessine_case (!pompier_y) (!pompier_x);
-      actualise_pompier (!pompier_x) (!pompier_y) j i;
-      pompier_x := j;
+      actualise_pompier (!pompier_x) (!pompier_y) j i; (*on stocke les coords dans la liste sous la forme (x,y)*)
+      pompier_x := j; (*(x,y) = (j,i)*)
       pompier_y := i;
-      dessine_case (i) (j);
+      dessine_case (i) (j); (*maj graphique*)
     end;;
 
 let changer_direction_vent () =
-	let num_vent = ref (match !wind_direction with
+	let num_vent = ref (match !wind_direction with (*conversion direction <=> entier (plus facile à manipuler)*)
 		| Haut -> 0
 		| Bas -> 4
 		| Gauche -> 2
@@ -227,9 +241,9 @@ let changer_direction_vent () =
 		| NO -> 1
 		| SE -> 5
 		| NE -> 7) in
-	let random = Random.int 8 in
+	let random = Random.int 8 in (*1 chance sur (8/2) que le vent change de direction (max conseille=10, min=3)*)
 	if (random = 0) then incr(num_vent)
-	else if (random = 1) then decr(num_vent);
+	else if (random = 1) then decr(num_vent); (*le vent change de direction de façon "continue"*)
 	num_vent := !num_vent mod 8;
 	wind_direction := (match !num_vent with
 	| 0 -> Haut
@@ -243,11 +257,10 @@ let changer_direction_vent () =
 	| _ ->Haut);;
 
 
-let unite_temps () =
+let unite_temps () = (*représente un tour de jeu*)
   let n = Array.length terrain in
   let m = Array.length terrain.(0) in
   let new_terrain = Array.make_matrix n m (init_case(Foret)) in
-  
   
   for i=0 to n-1 do
     for j=0 to m-1 do
@@ -255,16 +268,18 @@ let unite_temps () =
 			if (new_terrain.(i).(j).pompier > 0) then new_terrain.(i).(j).pompier <- 1;
     done;
   done;
-  
+  (*on fait une copie du terrain pour enflammer les cases correctement*)
   for i=0 to n-1 do
     for j=0 to m-1 do
+	(*pour chaque case on calcule les probas des 8 cases voisines de l'enflammer*)
       let feu = ref false in
       let pompier_a_cote = ref false in
       if (i>0) then
 	begin
 	  if (j>0) then
-	    if (terrain.(i-1).(j-1).pompier > 0) then pompier_a_cote := true else
-	      if (terrain.(i-1).(j-1).intensite_feu > 0 && (not(advanced_simulation) || (calcul_proba (terrain.(i-1).(j-1)) 25. 9) > (Random.float 100.)) ) then feu := true;
+	  (*case en haut à gauche*)
+	    if (terrain.(i-1).(j-1).pompier > 0) then pompier_a_cote := true else (*si il y a un pompier le comportement sera différent*)
+	      if (terrain.(i-1).(j-1).intensite_feu > 0 && (not(advanced_simulation) || (calcul_proba (terrain.(i-1).(j-1)) 25. 9) > (Random.float 100.)) ) then feu := true; (*le paramètre est 9 et non 1 car la case étudiée est en position 9 par rapport à la case en position 1!*)
 	  
 	  if (terrain.(i-1).(j).pompier > 0) then pompier_a_cote := true else
 	    if (terrain.(i-1).(j).intensite_feu > 0 && (not(advanced_simulation) || (calcul_proba (terrain.(i-1).(j)) 75. 8) > (Random.float 100.)) ) then feu := true;
@@ -300,7 +315,7 @@ let unite_temps () =
       
       if (!pompier_a_cote) then maj_feu new_terrain.(i).(j) 2
       else if (!feu) then
-	if new_terrain.(i).(j).element = Centrale then
+	if new_terrain.(i).(j).element = Centrale then (*explosion ou non?*)
 	  explosion new_terrain i j
 	else
 	  maj_feu new_terrain.(i).(j) 1;
@@ -314,7 +329,7 @@ let unite_temps () =
   done;
   
   changer_direction_vent ();
-  actualiser_vent ();
+  actualiser_vent (); (*maj graphique*)
   
   if (!foudre) then
     if not(tout_en_feu()) then
@@ -325,6 +340,7 @@ let unite_temps () =
 	  i_foudre := Random.int n;
 	  j_foudre := Random.int m;
 	done;
+	(*on prend des cases au hasard jusqu'à tomber sur une inflammable*)
 
 	allumer_feu terrain.(!i_foudre).(!j_foudre);
 	dessine_foudre (!i_foudre) (!j_foudre);
@@ -338,6 +354,7 @@ let imprimer_retry () =
 ;;
 
 let recommencer_partie () =
+	(*permet de refaire une partie sur le même terrain*)
   if (imprimer_retry()) then
     begin
       reinitialiser_terrain();
