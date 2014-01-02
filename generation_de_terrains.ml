26c0bb5c99d5c8c5c28ef6edb77edd5fa8738abc
Valpokbest <lefevre.valentin@gmail.com>
2014-01-02 21:30:06 +0100

    Update generation_de_terrains.ml

--- generation_de_terrains.ml
+++ generation_de_terrains.ml
@@ -6,6 +6,7 @@ open Types_et_donnees;;
 (* Initialisation de la graine *)
 Random.self_init();;
 
+(*une case par défaut avec un type de terrain*)
 let init_case (elt:element) =
   {element = elt; intensite_feu = 0; calcine = false; estompe = false; brule = false; pompier = 0;};;
 
@@ -13,12 +14,12 @@ let terrain_aleatoire hauteur largeur =
   let m = Array.make_matrix hauteur largeur (init_case(Foret)) in
   for i=0 to hauteur-1 do
     for j=0 to largeur-1 do
-      let random = Random.int 1000 in
-      if (random <= int_of_float(1000. *. prob_foret)) then
+      let random = Random.float 1. in
+      if (random <= prob_foret) then (*on respecte les probabilités pour les terrains*)
 	m.(i).(j) <- init_case(Foret)
-      else if (random <= int_of_float(1000. *. (prob_foret +. prob_plaine))) then
+      else if (random <= (prob_foret +. prob_plaine)) then
 	m.(i).(j) <- init_case(Plaine)
-      else if (random <= int_of_float(1000. *. (prob_foret +. prob_plaine +. prob_centrale))) then
+      else if (random <= (prob_foret +. prob_plaine +. prob_centrale)) then
 	m.(i).(j) <- init_case(Centrale)
       else
 	m.(i).(j) <- init_case(Eau);
@@ -26,6 +27,7 @@ let terrain_aleatoire hauteur largeur =
   done;
   m;;
 
+(*les maisons sont ajoutées par-dessus le terrain vague de la même manière*)
 let ajouter_maisons m = 
   let hauteur = Array.length m in
   let largeur = Array.length m.(0) in
@@ -37,6 +39,8 @@ let ajouter_maisons m =
     done;
   done;;
 
+(*Fonction qui permet de définir une norme sur les cases :
+	norme(m.(i1).(j1)) = card({x tq x.element = case1 & x voisine de m.(i1).(j1)}) *)
 let homogeneite m i1 j1 case1 =
   let hauteur = Array.length m in
   let largeur = Array.length m.(0) in
@@ -71,6 +75,7 @@ let homogeneite m i1 j1 case1 =
       incr(homogene);
   (!homogene);;
 
+(*pas dans l'algorithme d'unification du terrain*)
 let etape m =
   let hauteur = Array.length m in
   let largeur = Array.length m.(0) in
@@ -79,29 +84,33 @@ let etape m =
   let i2 = Random.int hauteur in
   let j2 = Random.int largeur in
   let case1 = m.(i1).(j1).element in
-  let case2 = m.(i2).(j2).element in
-  let homogene_avant = (homogeneite m i1 j1 case1) + (homogeneite m i2 j2 case2) in
+  let case2 = m.(i2).(j2).element in (* on sélectionne 2 cases aléatoirement (ou 1 si i1=i2 et j1=j2 mais c'est sans importance)*)
+  let homogene_avant = (homogeneite m i1 j1 case1) + (homogeneite m i2 j2 case2) in (*on calcule la norme des 2 cases avec leur élément*)
   
   m.(i1).(j1) <- init_case(case2);
-  m.(i2).(j2) <- init_case(case1);
+  m.(i2).(j2) <- init_case(case1); (*on échange*)
 
-  let homogene_apres = (homogeneite m i1 j1 case2) + (homogeneite m i2 j2 case1) in
+  let homogene_apres = (homogeneite m i1 j1 case2) + (homogeneite m i2 j2 case1) in (*on recalcule*)
   if (homogene_avant > homogene_apres) then (*si c'était mieux avant*)
     begin
       m.(i2).(j2) <- init_case(case2);
       m.(i1).(j1) <- init_case(case1);
     end;;
+	
+(*la norme ici est + grande si les cases autour sont de même élément que la case choisie
+donc après appel de étape le terrain est au moins autant homogène*)
 
 
 let terrain_intelligent m k =
   for i=1 to k do
     etape m;
   done;;
+(*on effectue etape plein de fois pour avoir un résultat correct*)
 
 let initialiser_terrain n m =
   let t = terrain_aleatoire n m in
   ajouter_maisons t;
-  terrain_intelligent t (n*m*100);
+  terrain_intelligent t (n*m*100); (*n*m*100 est un bon ordre de grandeur pour le nb d'étapes*)
 
 t;;
 
@@ -117,7 +126,7 @@ let allumer_terrain p =
     let j = Random.int m in
     let c = terrain.(i).(j) in
 
-    if c.intensite_feu = 0 && c.element <> Eau then (c.intensite_feu <- 1; decr k);
+    if c.intensite_feu = 0 && c.element <> Eau && c.element <> Maison then (c.intensite_feu <- 1; decr k);
     incr compteur;
   done;
 ;;
