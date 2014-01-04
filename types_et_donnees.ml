7c5aed9c1fdd07d286e05d3dfaae3707fc5f5f44
Valpokbest <lefevre.valentin@gmail.com>
2014-01-04 19:05:38 +0100

    Update types_et_donnees.ml

--- types_et_donnees.ml
+++ types_et_donnees.ml
@@ -1,4 +1,3 @@
-type element = Eau | Foret | Plaine | Maison | Centrale;;
 (* Résolution max de l'écran *)
 let max_x = 1280;;
 let max_y = 1024;;
@@ -10,11 +9,13 @@ type action = Pompier | Water | Feu;; (*Action de la souris*)
 
 type case = {
   element : element;				(*Type de la case*)
+  mutable contamine : bool;			(*true si la case est contaminée par le nuage radioactif *)
   mutable intensite_feu : int;
   mutable calcine : bool;			(*true si la case a fini de bruler*)
   mutable estompe : bool;			(*true si intensite_feu = intensite_max*)
   mutable brule : bool;				(*true si intensite_feu >= intensite_max/2*)
-  mutable pompier : int				(*0 = pas de pompier sur la case ; sinon valeur-1 = nb de pas effectués durant le tour (cad 4 = pompier coincé)*)
+  mutable pompier : int;			(*0 = pas de pompier sur la case ; sinon valeur-1 = nb de pas effectués durant le tour (cad 4 = pompier coincé)*)
+  mutable pv : int					(*Nombre de Points de Vie du pompier sur la case*)
 };;
 
 type terrain = case array array;;	(*matrice pour le terrain*)
@@ -32,7 +33,7 @@ let prob_foudre = 0.5;;
 
 (* Variables globales pour la simulation *)
 let wind_direction = ref SO;;
-let foudre = ref false;;
+let foudre = ref false;; (*cette variable sert à dire si la foudre doit frapper lors d'un tour*)
 let ia = ref false;;
 let compteur_tour = ref 1;;
 let action_souris = ref Pompier;;
@@ -41,6 +42,12 @@ let pompier_y = ref 0;;
 let compteur_pompiers = ref 0;;
 let liste_pompiers : ((int*int) list) ref = ref [];;	(*l'emplacement des pompiers est aussi géré sous forme de liste*)
 let fq_pomp = 10;;	(*Nb de tours avant arrivée d'un pompier*)
+let pv_pompier = 10;; (*Points de vie max d'un pompier*)
+let nuage : ((int*int) list) ref = ref [];; (*pour stocker les cases où le nuage radioactif se trouve*)
+
+let buffer = Array.create 10 'a';;
+let super_pompier = ref false;;
+let combinaison = ref false;;
 
 (*** Choix de l'utilisateurs ***)
 
@@ -76,11 +83,10 @@ let thunder = ( match read_line() with
     | "y" -> true
     | _ -> false);;
 
-
 print_endline "Choisissez une IA (1,2,3).";;
-let num_ia = ( match read_int() with
-|2 -> 2
-|3 -> 3
+let num_ia = ( match read_line() with
+|"2" -> 2
+|"3" -> 3
 |_ -> 1);;
 
 let advanced_simulation =
