4fe019f30a2ade3f7f77b00aa780c5aa141839b8
Valpokbest <lefevre.valentin@gmail.com>
2014-01-04 19:07:34 +0100

    Update generation_de_terrains.ml

--- generation_de_terrains.ml
+++ generation_de_terrains.ml
@@ -1,146 +1,99 @@
-(* #load "types_et_donnees.cmo";; *)
+(*#load "types_et_donnees.cmo";;
+#load "generation_de_terrains.cmo";;
+#load "fenetre_graphique.cmo";;*)
 (* à décommenter si non compilé *)
+
 open Types_et_donnees;;
+open Generation_de_terrains;;
+open Fenetre_graphique;;
 
-
-(* Initialisation de la graine *)
-Random.self_init();;
-
-(*une case par défaut avec un type de terrain*)
-let init_case (elt:element) =
-  {element = elt; contamine = false; intensite_feu = 0; calcine = false; estompe = false; brule = false; pompier = 0; pv = 0};;
-
-let terrain_aleatoire hauteur largeur =
-  let m = Array.make_matrix hauteur largeur (init_case(Foret)) in
-  for i=0 to hauteur-1 do
-    for j=0 to largeur-1 do
-      let random = Random.float 1. in
-      if (random <= prob_foret) then (*on respecte les probabilités pour les terrains*)
-	m.(i).(j) <- init_case(Foret)
-      else if (random <= (prob_foret +. prob_plaine)) then
-	m.(i).(j) <- init_case(Plaine)
-      else if (random <= (prob_foret +. prob_plaine +. prob_centrale)) then
-	m.(i).(j) <- init_case(Centrale)
-      else
-	m.(i).(j) <- init_case(Eau);
+let sauver carte num = 
+  let nb_rows=Array.length carte and nb_cols=Array.length carte.(0) in
+  let fichier = open_out ("Saves/"^string_of_int(num)^".map") in
+  (*on stocke diverses informations :
+  les compteurs pour la simulation*)
+  output_string fichier (string_of_int(!compteur_tour));
+  output_string fichier "\n";
+  output_string fichier (string_of_int(!compteur_pompiers));
+  output_string fichier "\n";
+  (*la taille du terrain*)
+  output_string fichier (string_of_int(nb_rows));
+  output_string fichier "\n";
+  output_string fichier (string_of_int(nb_cols));
+  output_string fichier "\n";
+  for i=0 to nb_rows-1 do
+    for j=0 to nb_cols-1 do
+	(*une ligne par case pour écrire tout l'enregistrement*)
+      let biome = match carte.(i).(j).element with
+	| Eau -> "00"
+	| Foret -> "01"
+	| Plaine -> "02"
+	| Maison -> "03"
+	| Centrale -> "04" in
+      output_string fichier biome;
+      if (carte.(i).(j).contamine) then output_string fichier "1" else output_string fichier "0";
+      output_string fichier (string_of_int(carte.(i).(j).intensite_feu));
+      if (carte.(i).(j).calcine) then output_string fichier "1" else output_string fichier "0";
+      if (carte.(i).(j).estompe) then output_string fichier "1" else output_string fichier "0";
+      if (carte.(i).(j).brule) then output_string fichier "1" else output_string fichier "0";
+      output_string fichier (string_of_int(carte.(i).(j).pompier));
+      output_string fichier "\n";
     done;
   done;
-  m;;
+  close_out fichier;;
 
-(*les maisons sont ajoutées par-dessus le terrain vague de la même manière*)
-let ajouter_maisons m = 
-  let hauteur = Array.length m in
-  let largeur = Array.length m.(0) in
-  for i=0 to hauteur-1 do
-    for j=0 to largeur-1 do
-      let random = Random.int 1000 in
-      if (random <= int_of_float(1000. *. prob_maison)) then
-	m.(i).(j) <- init_case(Maison);
-    done;
-  done;;
+let charger num = 
+  try (
+  let fichier = open_in ("Saves/"^string_of_int(num)^".map") in
+  let ct = int_of_string(input_line fichier) in
+  let cp = int_of_string(input_line fichier) in
+  let nb_rows = int_of_string(input_line fichier) in
+  let nb_cols = int_of_string(input_line fichier) in
+  liste_pompiers := [];
 
-(*Fonction qui permet de définir une norme sur les cases :
-	norme(m.(i1).(j1)) = card({x tq x.element = case1 & x voisine de m.(i1).(j1)}) *)
-let homogeneite m i1 j1 case1 =
-  let hauteur = Array.length m in
-  let largeur = Array.length m.(0) in
-  let homogene = ref 0 in
-  if (i1>0) then
+  if (nb_rows = n && nb_cols = m) then (*on vérifie que le terrain est de la même taille que celui chargé*)
     begin
-      if (j1>0) then
-	if (m.(i1-1).(j1-1).element = case1) then
-	  incr(homogene);
-      if (m.(i1-1).(j1).element = case1) then
-	incr(homogene);
-      if (j1<largeur-1) then
-	if (m.(i1-1).(j1+1).element = case1) then
-	  incr(homogene);
+      compteur_tour := ct;
+      actualiser_tour ();
+      compteur_pompiers := cp;
+      actualiser_nombre_pompiers ();
+      for i=0 to nb_rows-1 do
+	for j=0 to nb_cols-1 do
+	  let premier_code = String.make 1 (input_char fichier) in
+	  let deuxieme_code = String.make 1 (input_char fichier) in
+	  let code_biome = premier_code^deuxieme_code in (*code un peu long mais String.make 2 (input_char fichier)^(input_char fichier) ne fonctionne pas!*)
+	  let elt = (match code_biome with
+	    | "00" -> Eau
+	    | "01" -> Foret
+	    | "02" -> Plaine
+	    | "03" -> Maison
+	    | "04" -> Centrale
+	    | _ -> Eau) in
+	  let case = init_case(elt) in (*on recrée la case selon les infos stockées*)
+	  case.contamine <- (match (input_char fichier) with
+	    | '0' -> false
+	    | '1' -> true
+	    | _ -> false);
+	  case.intensite_feu <- int_of_string(String.make 1 (input_char fichier));
+	  let c = input_char fichier in
+	  case.calcine <- (match c with
+	    | '0' -> false
+	    | '1' -> true
+	    | _ -> false);
+	  case.estompe <- (match (input_char fichier) with
+	    | '0' -> false
+	    | '1' -> true
+	    | _ -> false);
+	  case.brule <- (match (input_char fichier) with
+	    | '0' -> false
+	    | '1' -> true
+	    | _ -> false);
+	  let pompier = int_of_string(String.make 1 (input_char fichier)) in
+	  case.pompier <- pompier;
+	  if (pompier > 0) then liste_pompiers := (j,i)::(!liste_pompiers); (*toujours gérer la double structure sur les pompiers*)
+	  let _ = input_line fichier in
+	  terrain.(i).(j) <- case;
+	done;
+      done;
     end;
-  if (j1<largeur-1) then
-    if (m.(i1).(j1+1).element = case1) then
-      incr(homogene);
-  if (i1<hauteur-1) then
-    begin
-      if (j1<largeur-1) then
-	if (m.(i1+1).(j1+1).element = case1) then
-	  incr(homogene);
-      if (m.(i1+1).(j1).element = case1) then
-	incr(homogene);
-      if (j1>0) then
-	if (m.(i1+1).(j1-1).element = case1) then
-	  incr(homogene);
-    end;
-  if (i1>0) then
-    if (m.(i1-1).(j1).element = case1) then
-      incr(homogene);
-  (!homogene);;
-
-(*pas dans l'algorithme d'unification du terrain*)
-let etape m =
-  let hauteur = Array.length m in
-  let largeur = Array.length m.(0) in
-  let i1 = Random.int hauteur in
-  let j1 = Random.int largeur in
-  let i2 = Random.int hauteur in
-  let j2 = Random.int largeur in
-  let case1 = m.(i1).(j1).element in
-  let case2 = m.(i2).(j2).element in (* on sélectionne 2 cases aléatoirement (ou 1 si i1=i2 et j1=j2 mais c'est sans importance)*)
-  let homogene_avant = (homogeneite m i1 j1 case1) + (homogeneite m i2 j2 case2) in (*on calcule la norme des 2 cases avec leur élément*)
-  
-  m.(i1).(j1) <- init_case(case2);
-  m.(i2).(j2) <- init_case(case1); (*on échange*)
-
-  let homogene_apres = (homogeneite m i1 j1 case2) + (homogeneite m i2 j2 case1) in (*on recalcule*)
-  if (homogene_avant > homogene_apres) then (*si c'était mieux avant*)
-    begin
-      m.(i2).(j2) <- init_case(case2);
-      m.(i1).(j1) <- init_case(case1);
-    end;;
-	
-(*la norme ici est + grande si les cases autour sont de même élément que la case choisie
-donc après appel de étape le terrain est au moins autant homogène*)
-
-
-let terrain_intelligent m k =
-  for i=1 to k do
-    etape m;
-  done;;
-(*on effectue etape plein de fois pour avoir un résultat correct*)
-
-let initialiser_terrain n m =
-  let t = terrain_aleatoire n m in
-  ajouter_maisons t;
-  terrain_intelligent t (n*m*100); (*n*m*100 est un bon ordre de grandeur pour le nb d'étapes*)
-
-t;;
-
-let terrain = initialiser_terrain n m;;
-
-let allumer_terrain p =
-(* Allume p cases du terrains *)
-  let k = ref p in
-  let compteur = ref 0 in (* vérif qu'on ne boucle pas à l'infini *)
-
-  while (!k > 0 && !compteur < n*m*10) do
-    let i = Random.int n in
-    let j = Random.int m in
-    let c = terrain.(i).(j) in
-
-    if c.intensite_feu = 0 && c.element <> Eau && c.element <> Maison then (c.intensite_feu <- 1; decr k);
-    incr compteur;
-  done;
-;;
-
-let reinitialiser_terrain () =
-  for i = 0 to (n-1) do
-    for j = 0 to (m-1) do
-      let c = terrain.(i).(j) in
-      c.intensite_feu <- 0;
-      c.estompe <- false;
-      c.brule <- false;
-      c.calcine <- false;
-      c.pompier <- 0;
-    done;
-  done;;
-
-allumer_terrain quantite_feu;;
+  ) with _ -> ();;
